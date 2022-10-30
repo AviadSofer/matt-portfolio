27bf61f5df501bf3eaf3b41851dbdecd9d34d438
AviadSofer <aviadsofer@gmail.com>
2022-10-30 15:04:06 +0200

    init project with all depends

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App: React.FC = () => {
+  return (
+    <div className='flex h-screen w-screen items-center justify-center bg-slate-800'>
+      <h2 className='text-center text-5xl font-bold text-yellow-50'>Welcome To My Project.</h2>
+    </div>
+  );
+};
+
+export default App;
