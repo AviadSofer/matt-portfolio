47a32c7407b52c23690dbbe8a599ff096d5cf7f1
AviadSofer <aviadsofer@gmail.com>
2022-11-02 20:27:49 +0200

    in About: insted flex- mx-auto, add py, add whitespace instead w-1/2

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
 const About: React.FC = () => {
   return (
-    <div className='flex h-screen flex-col items-center justify-center bg-primary text-center'>
-      <h2 className='text-3xl font-bold text-white'>Hi, I’m Matt. Nice to meet you.</h2>
-      <p className='w-1/2 py-7 text-xl font-semibold text-white'>
+    <div className='h-screen bg-primary py-36 text-center'>
+      <h2 className='mx-auto text-3xl font-bold text-white'>Hi, I’m Matt. Nice to meet you.</h2>
+      <p className='mx-auto whitespace-pre-line pt-7 text-xl font-semibold text-white'>
         {`Since beginning my journey as a freelance designer over 11 years ago, I've 
         done remote work for agencies, consulted for startups, and collaborated with 
         talented people to create digital products for both business and consumer 
